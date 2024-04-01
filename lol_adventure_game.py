01acc83087906657fc00ae9d2c7c8227060d8ef8
Gabrielle Lara <glara@terpmail.umd.edu>
2024-04-01 10:44:26 -0400

    added lol_adventure_game.py

---
+++ lol_adventure_game.py
@@ -0,0 +1,28 @@
+from argparse import ArgumentParser
+"""An adventure game that helps a player practice Python concepts by providing 
+questions for players to answer in each round of the game to answer to beat the monsters."""
+
+class Character():
+    def __init__():
+    
+    def __repr__():
+
+    def appearance():
+    
+    def pick_weapon():
+
+class Shop(Character):
+    def __init__():
+
+    def open_shop():
+    
+    def buy_item():
+
+class Monsters():
+    def __init__():
+    
+    def questions():
+
+def game_master():
+
+def parse_args(arglist):
